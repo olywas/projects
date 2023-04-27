8de6746d43a69ee4359094c71f325428810a32e5
olywas <olywier.was@gmail.com>
2023-04-28 00:34:36 +0200

    usuniecie wrażliwych danych

--- projekt hogwart/connbaza.php
+++ projekt hogwart/connbaza.php
@@ -1,6 +1,6 @@
 <?php
 //podłączenie bazy
-$baza = new mysqli("192.168.101.62", "labzsk_owasilewski", "uVw+kV1=(V5G", "labzsk_owasilewski");
+$baza = new mysqli("*******", "*********", "*********", "*********");
 
 if ($baza->connect_error) {
     die("Nie udało się połączyc z bazą danych: " . $baza->connect_error);
